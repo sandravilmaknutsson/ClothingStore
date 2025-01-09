3bdceae0eb56daa39aedde6303c3fe62d49b06bc
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2025-01-09 10:42:22 +0100

    Created a dummy database to have some data to work with

---
+++ Model/DummyDBCategory.cs
@@ -0,0 +1,12 @@
+namespace ClothingStore.Model
+{
+    public class DummyDBCategory
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        {
+            new Category{ CategoryId=1, Name="Shirts", Description="Shirts are mostly tight and thin in their fabric."},
+            new Category{CategoryId=2, Name="Pants", Description="Bottoms and pants in different fabrics"},
+            new Category{CategoryId=3, Name="Jackets", Description="Warmer pullovers and jackets for colder weather."}
+        };
+    }
+}
---
+++ Model/DummyDBProducts.cs
@@ -0,0 +1,13 @@
+namespace ClothingStore.Model
+{
+  public class DummyDBProduct
+  {
+    public IEnumerable<Product> AllProducts => new List<Product> {
+        new Product{ProductId=1,Name="T-Shirt", Brand="Zara", CategoryId=1, ImageUrl="test.jpg", Colors=["pink", "white", "blue"], Description="A simple T-shirt in beautiful colors.", Information="Material: 100% wool", IsInStock=true, Price=199, Rating="5/10", Sizes=["S", "M"] },
+          new Product{ProductId=2,Name="'Jeans'", Brand="H&M", CategoryId=2, ImageUrl="test.jpg", Colors=["Green", "white", "blue"], Description="Beautiful jeans in a variaty of colors.", Information="Material: 100% cotton", IsInStock=false, Price=599, Rating="8/10", Sizes=[] },
+            new Product{ProductId=3,Name="Leather Jacket", Brand="Zara", CategoryId=3, ImageUrl="test.jpg", Colors=["Black", "Brown",], Description="A cool leather jacket perfect for summer.", Information="Material: 100% leather", IsInStock=true, Price=349, Rating="6/10", Sizes=["S", "M", "L"] }
+     };
+
+    public Product? GetProductById(int productId) => AllProducts.FirstOrDefault(p => p.ProductId == productId);
+  }
+}

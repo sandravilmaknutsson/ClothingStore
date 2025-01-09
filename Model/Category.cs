9c1a53610b5f714ea32c67be69e9fb0c6e7730ed
Sandra Knutsson <sandra.knutsson@bahnhof.se>
2025-01-09 09:57:19 +0100

    Finished setting up product class and created a class for categories

---
+++ Model/Category.cs
@@ -0,0 +1,9 @@
+namespace ClothingStore.Model
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
--- Model/Product.cs
+++ Model/Product.cs
@@ -3,11 +3,17 @@ namespace ClothingStore.Model
     public class Product
     {
         public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = default!; // Shirt, Pants etc.
         public string Name { get; set; } = string.Empty;
+        public string? Brand { get; set; }
         public string? Description { get; set; }
         public int Price { get; set; }
-        public string? Category { get; set; }
+        public string? ImageUrl { get; set; }
         public bool IsInStock { get; set; }
-        public string? Sizes { get; set; }
+        public string? Rating { get; set; }
+        public List<string>? Colors { get; set; }
+        public List<string>? Sizes { get; set; }
+        public string? Information { get; set; } // Fabrics etc.
     }
 }
